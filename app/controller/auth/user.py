d041706308c7082d2c48d5d15399303531fb9694
hanrunan <hanrunan@corp.netease.com>
2024-02-04 11:22:57 +0800

    后端更改

--- app/controller/auth/user.py
+++ app/controller/auth/user.py
@@ -14,11 +14,11 @@ def register():
     data = request.get_json()
     username, password = data.get("username"), data.get("password")
     if not username or not password:
-        return jsonify(dict(status=101, msg="用户名或密码不能为空"))
+        return jsonify(dict(code=101, msg="用户名或密码不能为空"))
     email, name = data.get("email"), data.get("name")
     if not email or not name:
-        return jsonify(dict(status=101, msg="邮箱或姓名不能为空"))
+        return jsonify(dict(code=101, msg="邮箱或姓名不能为空"))
     err = UserDao.register_user(username, name, password, email) 
     if err is not None:
-        return jsonify(dict(status=101, msg=err))   
-    return jsonify(dict(status=0, msg="注册成功"))
+        return jsonify(dict(code=110, msg=err))   
+    return jsonify(dict(code=0, msg="注册成功"))
